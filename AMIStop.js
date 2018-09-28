baa5ef56e3034ae91dae1a98d2bdb3f1d84b5f6e
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-09-28 15:18:09 +0100

    Update AMIStop.js

--- AMIStop.js
+++ AMIStop.js
@@ -5,20 +5,16 @@
  */
 var settings = {
   'region': 'eu-west-2', // if you get an error like "region not found" or not supported remove the last letter the original region was eu-west-2a
-  'noreboot': true,
-  'ec2backuptagname': 'Backup',    // Add this tag to EC2 instances you wish to backup.
-  'ec2backuptagvalue': 'yes',
-}
-
-var aws = require('aws-sdk')
-aws.config.region = settings.region
-var ec2 = new aws.EC2()
-
+};
 var params = {
   InstanceIds: [
      "i-0f6fb8ed0a3aed833"
   ]
  };
+var aws = require('aws-sdk')
+aws.config.region = settings.region
+var ec2 = new aws.EC2()
+
  ec2.stopInstances(params, function(err, data) {
    if (err) console.log(err, err.stack); // an error occurred
    else     console.log(data);           // successful response
