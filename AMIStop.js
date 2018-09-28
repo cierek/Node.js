b8cf460e3b4222fa27e0002c32009b43a2e4396a
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-09-28 15:15:28 +0100

    Update AMIStop.js

--- AMIStop.js
+++ AMIStop.js
@@ -8,67 +8,35 @@ var settings = {
   'noreboot': true,
   'ec2backuptagname': 'Backup',    // Add this tag to EC2 instances you wish to backup.
   'ec2backuptagvalue': 'yes',
-  'amideletetagname': 'DeleteOn', // This tag name must match the one in the delete_amis.js script.
-  'amideletetagvalue': 'yes'
 }
 
 var aws = require('aws-sdk')
 aws.config.region = settings.region
 var ec2 = new aws.EC2()
-var now = new Date()
-date = now.toISOString().substring(0, 10)
-hours = now.getHours()
-minutes = now.getMinutes()
 
-exports.handler = function (event, context) {
-  var instanceparams = {
-    Filters: [{
-      Name: 'tag:' + settings.ec2backuptagname,
-      Values: [
-        settings.ec2backuptagvalue
-      ]
-    }]
-  }
-  ec2.describeInstances(instanceparams, function (err, data) {
-    if (err) console.log(err, err.stack)
-    else {
-      for (var i in data.Reservations) {
-        for (var j in data.Reservations[i].Instances) {
-          instanceid = data.Reservations[i].Instances[j].InstanceId
-          nametag = data.Reservations[i].Instances[j].Tags
-          for (var k in data.Reservations[i].Instances[j].Tags) {
-            if (data.Reservations[i].Instances[j].Tags[k].Key == 'Name') {
-              name = data.Reservations[i].Instances[j].Tags[k].Value
-            }
-          }
-          console.log('Creating AMIs of the Instance: ', name)
-          //preparing the name of the AMI
-          var imageparams = {
-            InstanceId: instanceid,
-            Name: name + '_' + date + '_' + hours + '-' + minutes,
-            NoReboot: settings.noreboot
-          }
-          //AMI creation
-          ec2.createImage(imageparams, function (err, data) {
-            if (err) console.log(err, err.stack)
-            else {
-              image = data.ImageId
-              console.log(image)
-              var tagparams = {
-                Resources: [image],
-                Tags: [{
-                  Key: settings.amideletetagname,
-                  Value: settings.amideletetagvalue
-                }]
-              }
-              ec2.createTags(tagparams, function (err, data) {
-                if (err) console.log(err, err.stack)
-                else console.log('Tags added to the created AMIs')
-              })
-            }
-          })
-        }
+var params = {
+  InstanceIds: [
+     "i-0f6fb8ed0a3aed833"
+  ]
+ };
+ ec2.stopInstances(params, function(err, data) {
+   if (err) console.log(err, err.stack); // an error occurred
+   else     console.log(data);           // successful response
+   
+   data = {
+    StoppingInstances: [
+       {
+      CurrentState: {
+       Code: 64, 
+       Name: "stopping"
+      }, 
+      InstanceId: "i-0f6fb8ed0a3aed833", 
+      PreviousState: {
+       Code: 16, 
+       Name: "running"
       }
-    }
-  })
-}
+     }
+    ]
+   }
+   
+ });
